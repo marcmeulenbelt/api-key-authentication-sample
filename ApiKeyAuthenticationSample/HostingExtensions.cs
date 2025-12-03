4cc02cb043b5c559e18a78abc4f7fef8d8a1edda
Marc Meulenbelt <marc.meulenbelt@frontiersoftware.com>
2025-12-03 12:02:28 +1100

    Refactors program startup into HostingExtensions
    Moves the application startup logic from Program.cs into extension methods on WebApplicationBuilder and WebApplication.
    
    This change improves code organization and promotes a cleaner Program.cs file, focusing on the essential bootstrapping process.

---
+++ ApiKeyAuthenticationSample/HostingExtensions.cs
@@ -0,0 +1,45 @@
+﻿using ApiKeyAuthenticationSample.Infrastructure;
+using static ApiKeyAuthenticationSample.Constants;
+
+namespace ApiKeyAuthenticationSample;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAuthentication(ApiKeyAuthentication.SchemeName)
+            .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthentication.SchemeName, _ => { });
+
+        builder.Services.AddSingleton<IApiKeyHashingService, Sha256ApiKeyHashingService>();
+        builder.Services.AddSingleton<IApiKeyStore, InMemoryApiKeyStore>();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
--- ApiKeyAuthenticationSample/Program.cs
+++ ApiKeyAuthenticationSample/Program.cs
@@ -1,36 +1,11 @@
-using ApiKeyAuthenticationSample.Infrastructure;
-using static ApiKeyAuthenticationSample.Constants;
+using ApiKeyAuthenticationSample;
+
+Console.Title = "API Key Authentication Sample";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(ApiKeyAuthentication.SchemeName)
-    .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>(ApiKeyAuthentication.SchemeName, options => { });
-
-builder.Services.AddSingleton<IApiKeyHashingService, Sha256ApiKeyHashingService>();
-builder.Services.AddSingleton<IApiKeyStore, InMemoryApiKeyStore>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+await app.RunAsync();
